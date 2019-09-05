e1cbe38d4799006e345286db7182f94c89997761
elizabeth109 <elizabeth109@mail.usf.edu>
2019-09-05 19:54:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * This C# Console application code is meant to introduce
+ * users to basic syntax of the language
+ */
+using System;
+
+namespace ISM4300HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //This is a change with one line comment
+            /*
+             This is a multi line
+             comment
+             */
+            Console.WriteLine("This is a change");
+        }
+    }
+}
